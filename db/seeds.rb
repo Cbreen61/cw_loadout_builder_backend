b5915b5908067a284e814877f10e7636682fc946
Christopher Breen <C.jbreen61@gmail.com>
2021-03-08 19:14:56 -0500

    Added seed data for  categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+metta_runner = Category.create(player_type: "Metta Runner")
+noob = Category.create(player_type: "Noob")
+camper = Category.create(player_type: "Camper")
+sniper = Category.create(player_type: "Sniper")
+pusher = Category.create(player_type: "Pusher")
+twitch_star = Category.create(player_type: "Twitch Star")
+testing = Category.create(player_type: "Testing")
