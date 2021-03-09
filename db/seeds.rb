ba17c222bfffc036702ae0bbaf98a6ba26001bf9
Christopher Breen <C.jbreen61@gmail.com>
2021-03-09 11:23:47 -0500

    added seed data for Primaryyw, secondaryw and loadout

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,7 @@ sniper = Category.create(player_type: "Sniper")
 pusher = Category.create(player_type: "Pusher")
 twitch_star = Category.create(player_type: "Twitch Star")
 testing = Category.create(player_type: "Testing")
+
+Primaryw.create(type:"Assault Rifle", name:"Kilo 141", attachment_1:"Muzzle: Flash Guard", attachment_2:"Optic: G.I. Mini", attachment_3:"Stock: No stock", attachment_4:"Laser: Tac laser", attachment_5:"Perk: FMJ",image_url:"https://cdn1.dotesports.com/wp-content/uploads/2020/07/15085821/codkilo1.jpg")
+Secondaryw.create(type:"Hand Gun", name:"1191", attachment_1:"Muzzle: Flash Guard", attachment_2:"Optic: G.I. Mini", attachment_3:"Tigger Action: Heavy Duty", attachment_4:"Laser: Tac laser", attachment_5:"Perk: FMJ",image_url:"https://gamewith-en.akamaized.net/article/thumbnail/rectangle/12656.png")
+Loadout.create(category_id: testing.id, name:"Tester", primaryw_id:1, secondaryw_id:1, perk_1:"Double Time", perk_2:"Hardline", perk_3:"Amped", lethal:"Claymore", tactical:"Stim")
